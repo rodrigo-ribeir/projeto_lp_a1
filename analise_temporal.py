f1544847295011d4761f9e649f5c21f5192cbac3
JDomingueti <jean.domingueti@gmail.com>
2024-10-03 18:17:26 -0300

    Adicionadas funções para juntar os dataframes e formatá-los

--- analise_temporal.py
+++ analise_temporal.py
@@ -2,6 +2,94 @@ import pandas as pd
 import basics as bs
 import matplotlib.pyplot as plt
 
+def normalize_prices(df: pd.DataFrame):
+    '''
+    
+    Normaliza os valores de todas as colunas que tiverem
+    a palavra 'Price' no nome. A normalização é feita no
+    próprio dataframe (inplace) dividindo cada valor dessas 
+    colunas pelo seu máximo.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        Dataframe com as colunas a serem normalizadas
+    
+    '''
+    columns = df.columns
+    for column in columns:
+        if 'Price' in str(column):
+            df[column] = df[column] / df[column].max()
+
+def rename_columns(df: pd.DataFrame, string):
+    '''
+
+    Função utilizada para adicionar uma palavra ao
+    nome das colunas de um DataFrame que é realizada
+    inplace, ou seja, altera o próprio DataFrame
+    passado como parâmetro. Caso haja uma coluna com 
+    nome `Date`, ela não é renomeada.
+
+    Parameters
+    ----------
+    df: pd.DataFrame
+        DataFrame com as colunas a serem renomeadas
+    
+    string: str
+        Palavra a ser adicionada no nome das colunas
+
+    '''
+    column_names = list(df.columns)
+    new_names = dict()
+    for name in column_names:
+        if (name == 'Date'):
+            continue
+        else:
+            new_names[name] = name + " " +  string
+    df.rename(columns=new_names, inplace=True)
+
+def agrupate_datasets(df1: pd.DataFrame, df2: pd.DataFrame, df1_name: str = 1, df2_name: str = 2) -> pd.DataFrame:
+    '''
+
+    Função utilizada para juntar dois datasets diferentes.
+    A junção é feita com base na coluna 'Date' dos datasets.
+
+    Parameters
+    ----------
+    df1: pd.Dataframe
+        DataFrame a ser mesclado
+
+    df2: pd.Dataframe
+        Dataframe a ser mesclado
+    
+    df1_name: str
+        Nome do primeiro DataFrame para personalização das 
+        colunas no DataFrame agrupado. O padrão é 1.
+    
+    df2_name: str
+        Nome do segundo DataFrame para personalização das 
+        colunas no DataFrame agrupado. O padrão é 2.
+
+    Returns
+    -------
+    df.DataFrame
+        DataFrame final com a mesclagem dos DataFrames 
+        passados como parâmetros com base nas datas dos
+        DataFrames. Caso um DataFrame não contenha o valor
+        nas mesmas datas que o outro, as colunas são 
+        preenchidas com 0.
+
+    '''
+    
+    rename_columns(df1, df1_name)
+    rename_columns(df2, df2_name)
+
+    df_result = pd.merge(df1, df2, on='Date', how='outer')
+    
+    df_result.fillna(0, inplace=True)
+
+    return df_result
+
 def agrupate_dates(df: pd.DataFrame, period: str = 'M') -> pd.DataFrame:
     '''
     Agrupa as datas em períodos de tempo pré-determinados
@@ -66,21 +154,44 @@ def agrupate_dates(df: pd.DataFrame, period: str = 'M') -> pd.DataFrame:
     dfc['End Date'] = e_dates
     return dfc
 
-df = bs.choose_dataset()
-colunas = ['Date', 'Price']
-df = bs.filtrar_colunas(df, colunas)
-df = bs.converter_dados(df)
-print("Por padrão, os dados são agrupados em meses.")
-period = 'M'
-ans = input("Você deseja mudar o período? [Y/N]\n -> ")
-if (ans.lower() == 'y'):
-    print("Períodos:\n [W] Semanas; [M] Meses; [B] Bimestres; [T] Trimestres; [S] Semestres; [Y] Anos")
-    ans = input("Digite o período que você deseja:\n -> ")
-    if not(ans.upper() in ['W', 'M', 'B', 'T', 'S', 'Y']):
-        print("Período inválido.")
-        quit()
-    else: period = ans
+# Para não rodar o script sem ser o principal
+if __name__ == "__main__":
+    # Colunas trabalhadas
+    colunas = ['Date', 'Price']
+    df1, nome1 = bs.choose_dataset(2, True)
+    df1 = bs.filtrar_colunas(df1, colunas)
+    df1 = bs.converter_dados(df1)
+    df2, nome2 = bs.choose_dataset(3, True)
+    df2 = bs.filtrar_colunas(df2, colunas)
+    df2 = bs.converter_dados(df2)
 
-df = agrupate_dates(df, period)
-df.plot(x='Start Date', y='Price', kind='line')
-plt.show()
+    final_df = agrupate_datasets(df1, df2, nome1, nome2)
+
+
+    print("Por padrão, os dados são agrupados em meses.")
+    period = 'M'
+    ans = input("Você deseja mudar o período? [Y/N]\n -> ")
+    if (ans.lower() == 'y'):
+        print("Períodos:\n [W] Semanas; [M] Meses; [B] Bimestres; [T] Trimestres; [S] Semestres; [Y] Anos")
+        ans = input("Digite o período que você deseja:\n -> ")
+        if not(ans.upper() in ['W', 'M', 'B', 'T', 'S', 'Y']):
+            print("Período inválido.")
+            quit()
+        else: period = ans
+
+    # Agrupa os dados pelas datas no período determinado
+    final_df = agrupate_dates(final_df, period)
+
+    # Divide cada coluna de preço por seu máximo para ver o maior período de crescimento
+    # final_df = 
+    normalize_prices(final_df)
+
+    # Verifica quais colunas serão plotadas no gráfico
+    price_columns = list()
+    for column in list(final_df.columns):
+        if 'Price' in str(column):
+            price_columns.append(column)
+
+    final_df.plot(x='Start Date', y=price_columns, kind='line')
+    plt.show()
+    
